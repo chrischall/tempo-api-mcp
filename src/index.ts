7e3f2023f099f3b16a535530c13e957c8c3745cd
chrischall <chris.c.hall@gmail.com>
2026-07-13 23:39:04 +0200

    chore(main): release 2.1.8

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const client = new TempoClient();
 
 await runMcp({
   name: 'tempo-api-mcp',
-  version: '2.1.7', // x-release-please-version
+  version: '2.1.8', // x-release-please-version
   deps: client,
   tools: [
     registerWorklogs,
