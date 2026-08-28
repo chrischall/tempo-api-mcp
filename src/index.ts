94e9e2ca7b95fa276c4d17fcdd70dca9a911915f
chrischall <chris.c.hall@gmail.com>
2026-08-28 07:33:51 -0400

    chore(main): release 2.3.2 (#119)
    :robot: I have created a release *beep* *boop*
    ---
    
    
    ##
    [2.3.2](https://github.com/chrischall/tempo-api-mcp/compare/v2.3.1...v2.3.2)
    (2026-08-27)
    
    
    ### Documentation
    
    * npm test now typechecks before running vitest
    ([#118](https://github.com/chrischall/tempo-api-mcp/issues/118))
    ([59b9df3](https://github.com/chrischall/tempo-api-mcp/commit/59b9df3e9996bafce97fc4d467aeec84199994b1))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const client = new TempoClient();
 
 await runMcp({
   name: 'tempo-api-mcp',
-  version: '2.3.1', // x-release-please-version
+  version: '2.3.2', // x-release-please-version
   deps: client,
   tools: [
     registerWorklogs,
