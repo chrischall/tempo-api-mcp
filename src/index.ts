3fe3347b6c5767f9539f84768810578aa357b500
chrischall <chris.c.hall@gmail.com>
2026-09-24 11:07:16 -0400

    chore(main): release 3.2.0 (#177)
    :robot: I have created a release *beep* *boop*
    ---
    
    
    ##
    [3.2.0](https://github.com/chrischall/tempo-api-mcp/compare/v3.1.2...v3.2.0)
    (2026-09-24)
    
    
    ### Features
    
    * confirm writes with a preview token instead of confirm: true
    ([#176](https://github.com/chrischall/tempo-api-mcp/issues/176))
    ([1b8d6c4](https://github.com/chrischall/tempo-api-mcp/commit/1b8d6c4b2e25ffe9858a8bea1295cd69fd4573ac))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const client = new TempoClient();
 
 await runMcp({
   name: 'tempo-api-mcp',
-  version: '3.1.2', // x-release-please-version
+  version: '3.2.0', // x-release-please-version
   deps: client,
   tools: [
     registerWorklogs,
